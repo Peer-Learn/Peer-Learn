75d0bd87f164680a2feed7e3914a7c57bd94180b
Peer-Learn <128922404+Peer-Learn@users.noreply.github.com>
2023-03-25 23:34:54 +0530

    Created Landing Page
    Peer Learn Website - Task by Asper

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("op");
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+function toogleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
